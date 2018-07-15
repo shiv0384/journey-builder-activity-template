a9cd76ab435e32bb2a51bab1ebd6a8d9c089d7b4
shiv0384 <41044425+shiv0384@users.noreply.github.com>
2018-07-15 20:41:46 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -43,15 +43,7 @@ define([
             $('#message').html(message);
         });
 
-        // Toggle step 4 active/inactive
-        // If inactive, wizard hides it and skips over it during navigation
-        $('#toggleLastStep').click(function() {
-            lastStepEnabled = !lastStepEnabled; // toggle status
-            steps[3].active = !steps[3].active; // toggle active
-
-            connection.trigger('updateSteps', steps);
-        });
-    }
+        
 
     function initialize (data) {
         if (data) {
@@ -77,106 +69,7 @@ define([
         });
 
         // If there is no message selected, disable the next button
-        if (!message) {
-            showStep(null, 1);
-            connection.trigger('updateButton', { button: 'next', enabled: false });
-            // If there is a message, skip to the summary step
-        } else {
-            $('#select1').find('option[value='+ message +']').attr('selected', 'selected');
-            $('#message').html(message);
-            showStep(null, 3);
-        }
-    }
-
-    function onGetTokens (tokens) {
-        // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
-        // console.log(tokens);
-    }
-
-    function onGetEndpoints (endpoints) {
-        // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
-        // console.log(endpoints);
-    }
-
-    function onClickedNext () {
-        if (
-            (currentStep.key === 'step3' && steps[3].active === false) ||
-            currentStep.key === 'step4'
-        ) {
-            save();
-        } else {
-            connection.trigger('nextStep');
-        }
-    }
-
-    function onClickedBack () {
-        connection.trigger('prevStep');
-    }
-
-    function onGotoStep (step) {
-        showStep(step);
-        connection.trigger('ready');
-    }
-
-    function showStep(step, stepIndex) {
-        if (stepIndex && !step) {
-            step = steps[stepIndex-1];
-        }
-
-        currentStep = step;
-
-        $('.step').hide();
-
-        switch(currentStep.key) {
-            case 'step1':
-                $('#step1').show();
-                connection.trigger('updateButton', {
-                    button: 'next',
-                    enabled: Boolean(getMessage())
-                });
-                connection.trigger('updateButton', {
-                    button: 'back',
-                    visible: false
-                });
-                break;
-            case 'step2':
-                $('#step2').show();
-                connection.trigger('updateButton', {
-                    button: 'back',
-                    visible: true
-                });
-                connection.trigger('updateButton', {
-                    button: 'next',
-                    text: 'next',
-                    visible: true
-                });
-                break;
-            case 'step3':
-                $('#step3').show();
-                connection.trigger('updateButton', {
-                     button: 'back',
-                     visible: true
-                });
-                if (lastStepEnabled) {
-                    connection.trigger('updateButton', {
-                        button: 'next',
-                        text: 'next',
-                        visible: true
-                    });
-                } else {
-                    connection.trigger('updateButton', {
-                        button: 'next',
-                        text: 'done',
-                        visible: true
-                    });
-                }
-                break;
-            case 'step4':
-                $('#step4').show();
-                break;
-        }
-    }
-
+        
     function save() {
         var name = $('#select1').find('option:selected').html();
         var value = getMessage();
