603186e1ebeb3ef94690467e1b9eed077bdc10f0
shiv0384 <41044425+shiv0384@users.noreply.github.com>
2018-07-17 16:28:11 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -9,7 +9,7 @@ define([
     var payload = {};
     var lastStepEnabled = false;
 	var steps = [ // initialize to the same value as what's set in config.json for consistency
-				{ "label": "Step 1", "key": "step1" },
+				{ "label": "Configure SMS", "key": "step1" },
         
 				];
 	var currentStep = steps[0].key;
