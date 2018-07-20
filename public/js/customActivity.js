b00d6c6a27d4b945067828f7b69c6d6f3cca6cf1
shiv0384 <41044425+shiv0384@users.noreply.github.com>
2018-07-20 19:13:02 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -54,16 +54,51 @@ define(function (require) {
 	}
 
 	function save () {
-		var eventDefinitionKey = $('#select-entryevent-defkey').val();
+	payload['arguments'].execute.inArguments = [{
+            "tokens": authTokens,
+            "FirstName": "{{Contact.Attribute.sendSmsData.FirstName}}",
+            "LastName": "{{Contact.Attribute.sendSmsData.LastName}}",
+            "PhoneNumber": "{{Contact.Attribute.sendSmsData.PhoneNumber}}",
+             "EmailAddress": "{{Contact.Attribute.sendSmsData.EmailAddress}}"
+		var d1={
+					"strMobileNumber":"{{Contact.Attribute.sendSmsData.PhoneNumber}}",
+					"strTxtMsg":"This is simple message"
+		 };
+		 var tokendata;
+		 $.ajax({
+		 
+		 url:"https://login.salesforce.com/services/oauth2/token?client_id=3MVG9sG9Z3Q1RlbeZ3x_5JrzSFxlATWV7amV.1PtetznXcMooCQBQHBf6YgcAQDJtSy317Zpo4kevq_65cbGI&client_secret=5906482776105122251&username=pavani.jidagam@opensms.com&password=Appshark7&grant_type=password ",
+		 method:"post",
+		 dataType:"json",
+		 cache:false,
+		 async:false,
+		 success:function(result){
+			 tokendata= result.access_token;
+		 },
+		 error:function(){
+		 console.log("error");
+		 } 
 
-		payload['arguments'] = payload['arguments'] || {};
-		payload['arguments'].execute = payload['arguments'].execute || {};
-		payload['arguments'].execute.inArguments = [{
-			'serviceCloudId': '{{Event.' + eventDefinitionKey + '.\"Contact:Id\"}}'
-		}];
+		 });
+		//Value of the text box 
+		$.ajax({
+		 url: "https://appsharkopenmsg-developer-edition.na24.force.com/services/apexrest/OpenSMSPro/MarketingCloudSendSMS",
+		  method: "post",
+		  async: false,
+		  contentType: "application/json",	
+		   data: JSON.stringify(d1),
+	     beforeSend : function( xhr ) {
+				xhr.setRequestHeader('Authorization', 'Bearer ' + tokendata);
+			},
+		 success:function(result,status){
+				connection.trigger('ready');	 
+		 },
+		 error:function(res){
+		 console.log("Error");
+		 
+		 } 
 
-		payload['metaData'] = payload['metaData'] || {};
-		payload['metaData'].isConfigured = true;
+		 });
 
 		console.log(JSON.stringify(payload));
 
