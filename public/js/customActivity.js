ded4a6b6f604fab61936fea4f4e456411fbb30f6
shiv0384 <41044425+shiv0384@users.noreply.github.com>
2018-07-20 16:41:15 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,7 +1,7 @@
 define([
     'postmonger'
 ], function (
-    Postmonger 
+    Postmonger
 ) {
     'use strict';
 
@@ -22,7 +22,7 @@ define([
 
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
- 
+
     }
 
     function initialize(data) {
@@ -64,61 +64,22 @@ define([
     function onGetEndpoints(endpoints) {
         console.log(endpoints);
     }
-	var deFields = [];
 
     function save() {
-       
               payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
             "FirstName": "{{Contact.Attribute.sendSmsData.FirstName}}",
             "LastName": "{{Contact.Attribute.sendSmsData.LastName}}",
             "PhoneNumber": "{{Contact.Attribute.sendSmsData.PhoneNumber}}",
              "EmailAddress": "{{Contact.Attribute.sendSmsData.EmailAddress}}"
-           
-        }];
-       		 var d1={
-					"strMobileNumber":"8452881510",
-					"strTxtMsg":"This is simple message"
-		 };
-		 var tokendata;
-		 $.ajax({
-		 
-		 url:"https://login.salesforce.com/services/oauth2/token?client_id=3MVG9sG9Z3Q1RlbeZ3x_5JrzSFxlATWV7amV.1PtetznXcMooCQBQHBf6YgcAQDJtSy317Zpo4kevq_65cbGI&client_secret=5906482776105122251&username=pavani.jidagam@opensms.com&password=Appshark7&grant_type=password ",
-		 method:"post",
-		 dataType:"json",
-		 cache:false,
-		 async:false,
-		 success:function(result){
-			 tokendata= result.access_token;
-		 },
-		 error:function(){
-		 console.log("error");
-		 } 
-
-		 });
-		//Value of the text box 
-		$.ajax({
-		 url: "https://appsharkopenmsg-developer-edition.na24.force.com/services/apexrest/OpenSMSPro/MarketingCloudSendSMS",
-		  method: "post",
-		  async: false,
-		  contentType: "application/json",	
-		   data: JSON.stringify(d1),
-	     beforeSend : function( xhr ) {
-				xhr.setRequestHeader('Authorization', 'Bearer ' + tokendata);
-			},
-		 success:function(result,status){
 			 
-		 },
-		 error:function(res){
-		 console.log("Error");
-		 
-		 } 
+        }];
+        
+        payload['metaData'].isConfigured = true;
 
-		 });
-     	 payload['metaData'].isConfigured = true;
-       connection.trigger('updateActivity', payload);
+        console.log(payload);
+        connection.trigger('updateActivity', payload);
     }
 
 
-	
 });
