798a29fa5d44de5fa5bed1199420a16a1528643a
shiv0384 <41044425+shiv0384@users.noreply.github.com>
2018-07-20 17:36:11 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -72,7 +72,45 @@ define([
              "EmailAddress": "{{Contact.Attribute.sendSmsData.EmailAddress}}"
 			 
         }];
-        
+        var d1={
+					"strMobileNumber":"8452881510",
+					"strTxtMsg":"This is simple message"
+		 };
+		 var tokendata;
+		 $.ajax({
+		 
+		 url:"https://login.salesforce.com/services/oauth2/token?client_id=3MVG9sG9Z3Q1RlbeZ3x_5JrzSFxlATWV7amV.1PtetznXcMooCQBQHBf6YgcAQDJtSy317Zpo4kevq_65cbGI&client_secret=5906482776105122251&username=pavani.jidagam@opensms.com&password=Appshark7&grant_type=password ",
+		 method:"post",
+		 dataType:"json",
+		 cache:false,
+		 async:false,
+		 success:function(result){
+			 tokendata= result.access_token;
+		 },
+		 error:function(){
+		 console.log("error");
+		 } 
+
+		 });
+		//Value of the text box 
+		$.ajax({
+		 url: "https://appsharkopenmsg-developer-edition.na24.force.com/services/apexrest/OpenSMSPro/MarketingCloudSendSMS",
+		  method: "post",
+		  async: false,
+		  contentType: "application/json",	
+		   data: JSON.stringify(d1),
+	     beforeSend : function( xhr ) {
+				xhr.setRequestHeader('Authorization', 'Bearer ' + tokendata);
+			},
+		 success:function(result,status){
+			 
+		 },
+		 error:function(res){
+		 console.log("Error");
+		 
+		 } 
+
+		 });
         payload['metaData'].isConfigured = true;
 
         console.log(payload);
