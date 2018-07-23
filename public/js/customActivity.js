bd02917eff93543200b7e61f8a35e9d5bd854b1e
shiv0384 <41044425+shiv0384@users.noreply.github.com>
2018-07-23 13:29:28 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -64,62 +64,63 @@ define([
         console.log(endpoints);
     }
 
-    function save() {
-	    debugger;
-	    payload['arguments'].execute.inArguments = [{
-            "tokens": authTokens,
-            "FirstName": "{{Contact.Attribute.sendSmsData.FirstName}}",
-	     "LastName": "{{Contact.Attribute.sendSmsData.LastName}}",
-	      "PhoneNumber": "{{Contact.Attribute.sendSmsData.PhoneNumber}}",
-	      "EmailAddress": "{{Contact.Attribute.sendSmsData.EmailAddress}}"
+           function save()
+		   {
+				debugger;
+				payload['arguments'].execute.inArguments = [{
+				"tokens": authTokens,
+				"FirstName": "{{Contact.Attribute.sendSmsData.FirstName}}",
+				"LastName": "{{Contact.Attribute.sendSmsData.LastName}}",
+				"PhoneNumber": "{{Contact.Attribute.sendSmsData.PhoneNumber}}",
+				"EmailAddress": "{{Contact.Attribute.sendSmsData.EmailAddress}}"
 
-        }];
-              
-       		debugger;			
-        var d1={
-					"strMobileNumber":"{{Contact.Attribute.sendSmsData.PhoneNumber}}",
-					"strTxtMsg":"This is simple message"
-		 };
-	         debugger;
-		 var tokendata;
-		 $.ajax({
-		 
-		 url:"https://login.salesforce.com/services/oauth2/token?client_id=3MVG9sG9Z3Q1RlbeZ3x_5JrzSFxlATWV7amV.1PtetznXcMooCQBQHBf6YgcAQDJtSy317Zpo4kevq_65cbGI&client_secret=5906482776105122251&username=pavani.jidagam@opensms.com&password=Appshark7&grant_type=password ",
-		 method:"post",
-		 dataType:"json",
-		 cache:false,
-		 async:false,
-		 success:function(result){
-			 tokendata= result.access_token;
-		 },
-		 error:function(){
-		 console.log("error");
-		 } 
+				}];
 
-		 });
-		//Value of the text box 
-		$.ajax({
-		 url: "https://appsharkopenmsg-developer-edition.na24.force.com/services/apexrest/OpenSMSPro/MarketingCloudSendSMS",
-		  method: "post",
-		  async: false,
-		  contentType: "application/json",	
-		   data: JSON.stringify(d1),
-	     beforeSend : function( xhr ) {
+				debugger;			
+				var phonemsgdata={
+				"strMobileNumber":"{{Contact.Attribute.sendSmsData.PhoneNumber}}",
+				"strTxtMsg":"This is simple message"
+				};
+				debugger;
+				var tokendata;
+				$.ajax({
+
+				url:"https://login.salesforce.com/services/oauth2/token?client_id=3MVG9sG9Z3Q1RlbeZ3x_5JrzSFxlATWV7amV.1PtetznXcMooCQBQHBf6YgcAQDJtSy317Zpo4kevq_65cbGI&client_secret=5906482776105122251&username=pavani.jidagam@opensms.com&password=Appshark7&grant_type=password ",
+				method:"post",
+				dataType:"json",
+				cache:false,
+				async:false,
+				success:function(result){
+				tokendata= result.access_token;
+				},
+				error:function(){
+				console.log("error");
+				} 
+
+				});
+				//Value of the text box 
+				$.ajax({
+				url: "https://appsharkopenmsg-developer-edition.na24.force.com/services/apexrest/OpenSMSPro/MarketingCloudSendSMS",
+				method: "post",
+				async: false,
+				contentType: "application/json",	
+				data: JSON.stringify(phonemsgdata),
+				beforeSend : function( xhr ) {
 				xhr.setRequestHeader('Authorization', 'Bearer ' + tokendata);
-			},
-		 success:function(result,status){
+				},
+				success:function(result,status){
 				connection.trigger('ready');	 
-		 },
-		 error:function(res){
-		 console.log("Error");
-		 
-		 } 
+				},
+				error:function(res){
+				console.log("Error");
 
-		 });
-        payload['metaData'].isConfigured = true;
+				} 
 
-        console.log(payload);
-        connection.trigger('updateActivity', payload);
+				});
+				payload['metaData'].isConfigured = true;
+
+				console.log(payload);
+				connection.trigger('updateActivity', payload);
 	
     }
 
