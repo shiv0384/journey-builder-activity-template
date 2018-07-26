f77da3f8a5ae8fd0f53b4afbcc92e40dcb0b763c
shiv0384 <41044425+shiv0384@users.noreply.github.com>
2018-07-26 18:29:47 +0530

    Update customActivity.js

--- public/customActivity.js
+++ public/customActivity.js
@@ -1,128 +1,130 @@
-'use strict';
+debugger;
+define([
+    'postmonger'
+], function (
+    Postmonger
+) {
+    'use strict';
 
-define(function (require) {
-	var Postmonger = require('postmonger');
-	var connection = new Postmonger.Session();
-	var payload = {};
-	var steps = [
-		{'key': 'eventdefinitionkey', 'label': 'Event Definition Key'},
-		{'key': 'idselection', 'label': 'ID Selection'}
-	];
-	var currentStep = steps[0].key;
-	var eventDefinitionKey = '';
-	var deFields = [];
+    var connection = new Postmonger.Session();
+    var authTokens = {};
+    var payload = {};
+    $(window).ready(onRender);
 
-	$(window).ready(function () {
-		connection.trigger('ready');
-		connection.trigger('requestInteraction');
-	});
+    connection.on('initActivity', initialize);
+    connection.on('requestedTokens', onGetTokens);
+    connection.on('requestedEndpoints', onGetEndpoints);
+    connection.on('clickedNext', save);
+   
+    function onRender() {
+        // JB will respond the first time 'ready' is called with 'initActivity'
+        connection.trigger('ready');
 
-	function initialize (data) {
-		if (data) {
-			payload = data;
-		}
-	}
+        connection.trigger('requestTokens');
+        connection.trigger('requestEndpoints');
 
-	function onClickedNext () {
-		if (currentStep.key === 'idselection') {
-			save();
-		} else {
-			connection.trigger('nextStep');
-		}
-	}
+    }
 
-	function onClickedBack () {
-		connection.trigger('prevStep');
-	}
+    function initialize(data) {
+        console.log(data);
+        if (data) {
+            payload = data;
+        }
+        
+        var hasInArguments = Boolean(
+            payload['arguments'] &&
+            payload['arguments'].execute &&
+            payload['arguments'].execute.inArguments &&
+            payload['arguments'].execute.inArguments.length > 0
+        );
 
-	function onGotoStep (step) {
-		showStep(step);
-		connection.trigger('ready');
-	}
+        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-	function showStep (step, stepIndex) {
-		if (stepIndex && !step) {
-			step = steps[stepIndex - 1];
-		}
+        console.log(inArguments);
 
-		currentStep = step;
+        $.each(inArguments, function (index, inArgument) {
+            $.each(inArgument, function (key, val) {
+                
+              
+            });
+        });
 
-		$('.step').hide();
+        connection.trigger('updateButton', {
+            button: 'next',
+            text: 'done',
+            visible: true
+        });
+    }
 
-		switch (currentStep.key) {
-		case 'eventdefinitionkey':
-			$('#step1').show();
-			$('#step1 input').focus();
-			break;
-		case 'idselection':
-			$('#step2').show();
-			$('#step2 input').focus();
-			break;
-		}
-	}
+    function onGetTokens(tokens) {
+        console.log(tokens);
+        authTokens = tokens;
+    }
 
-	function requestedInteractionHandler (settings) {
-		try {
-			eventDefinitionKey = settings.triggers[0].metaData.eventDefinitionKey;
-			$('#select-entryevent-defkey').val(eventDefinitionKey);
+    function onGetEndpoints(endpoints) {
+        console.log(endpoints);
+    }
 
-			if (settings.triggers[0].type === 'SalesforceObjectTriggerV2' &&
-					settings.triggers[0].configurationArguments &&
-					settings.triggers[0].configurationArguments.eventDataConfig) {
+           function save()
+		   {
+				debugger;
+				payload['arguments'].execute.inArguments = [{
+				"tokens": authTokens,
+				"FirstName": "{{Contact.Attribute.BCA57FAA-A16D-428B-80DC-BA5FBEB5DCC3.FirstName}}",
+				"LastName": "{{Contact.Attribute.BCA57FAA-A16D-428B-80DC-BA5FBEB5DCC3.LastName}}",
+				"PhoneNumber": "{{Contact.Attribute.BCA57FAA-A16D-428B-80DC-BA5FBEB5DCC3.PhoneNumber}}",
+				"EmailAddress": "{{Contact.Attribute.BCA57FAA-A16D-428B-80DC-BA5FBEB5DCC3.EmailAddress}}"
 
-				// This workaround is necessary as Salesforce occasionally returns the eventDataConfig-object as string
-				if (typeof settings.triggers[0].configurationArguments.eventDataConfig === 'stirng' ||
-							!settings.triggers[0].configurationArguments.eventDataConfig.objects) {
-						settings.triggers[0].configurationArguments.eventDataConfig = JSON.parse(settings.triggers[0].configurationArguments.eventDataConfig);
-				}
+				}];
 
-				settings.triggers[0].configurationArguments.eventDataConfig.objects.forEach((obj) => {
-					deFields = deFields.concat(obj.fields.map((fieldName) => {
-						return obj.dePrefix + fieldName;
-					}));
+				debugger;			
+				var phonemsgdata={
+				"strMobileNumber":"{{Contact.Attribute.BCA57FAA-A16D-428B-80DC-BA5FBEB5DCC3.PhoneNumber}}",
+				"strTxtMsg":"Test message for sms"
+				};
+				debugger;
+				var tokendata;
+				$.ajax({
+
+				url:"https://login.salesforce.com/services/oauth2/token?client_id=3MVG9sG9Z3Q1RlbeZ3x_5JrzSFxlATWV7amV.1PtetznXcMooCQBQHBf6YgcAQDJtSy317Zpo4kevq_65cbGI&client_secret=5906482776105122251&username=pavani.jidagam@opensms.com&password=Appshark7&grant_type=password ",
+				method:"post",
+				dataType:"json",
+				cache:false,
+				async:false,
+				success:function(result){
+				tokendata= result.access_token;
+				},
+				error:function(){
+				console.log("error");
+				} 
+
 				});
+				//Value of the text box 
+				$.ajax({
+				url: "https://appsharkopenmsg-developer-edition.na24.force.com/services/apexrest/OpenSMSPro/MarketingCloudSendSMS",
+				method: "post",
+				async: false,
+				contentType: "application/json",	
+				data: JSON.stringify(phonemsgdata),
+				beforeSend : function( xhr ) {
+				xhr.setRequestHeader('Authorization', 'Bearer ' + tokendata);
+				},
+				success:function(result,status){
+				connection.trigger('ready');	 
+				},
+				error:function(res){
+				console.log("Error");
 
-				deFields.forEach((option) => {
-					$('#select-id-dropdown').append($('<option>', {
-						value: option,
-						text: option
-					}));
+				} 
+
 				});
+				payload['metaData'].isConfigured = true;
 
-				$('#select-id').hide();
-				$('#select-id-dropdown').show();
-			} else {
-				$('#select-id-dropdown').hide();
-				$('#select-id').show();
-			}
-		} catch (e) {
-			console.error(e);
-			$('#select-id-dropdown').hide();
-			$('#select-id').show();
-		}
-	}
+				console.log(payload);
+				connection.trigger('updateActivity', payload);
+	
+    }
+	
 
-	function save () {
-		payload['arguments'] = payload['arguments'] || {};
-		payload['arguments'].execute = payload['arguments'].execute || {};
 
-		var idField = deFields.length > 0 ? $('#select-id-dropdown').val() : $('#select-id').val();
-
-		payload['arguments'].execute.inArguments = [{
-			'serviceCloudId': '{{Event.' + eventDefinitionKey + '.\"' + idField + '\"}}'
-		}];
-
-		payload['metaData'] = payload['metaData'] || {};
-		payload['metaData'].isConfigured = true;
-
-		console.log(JSON.stringify(payload));
-
-		connection.trigger('updateActivity', payload);
-	}
-
-	connection.on('initActivity', initialize);
-	connection.on('clickedNext', onClickedNext);
-	connection.on('clickedBack', onClickedBack);
-	connection.on('gotoStep', onGotoStep);
-	connection.on('requestedInteraction', requestedInteractionHandler);
 });
