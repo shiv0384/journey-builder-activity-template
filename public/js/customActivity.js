1c7a2df4872475d4ba55fae2d2bc72a80e162476
shiv0384 <41044425+shiv0384@users.noreply.github.com>
2018-08-02 12:51:31 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -7,14 +7,16 @@ define([
 
     var connection = new Postmonger.Session();
     var authTokens = {};
-	var payload = {};
-	$(window).ready(onRender);
-	connection.on('initActivity', initialize);
-	 connection.on('requestedTokens', onGetTokens);
-	
-	       
-	 connection.on('clickedNext', save);
-	 function onRender() {
+    var payload = {};
+    $(window).ready(onRender);
+
+    connection.on('initActivity', initialize);
+    connection.on('requestedTokens', onGetTokens);
+    connection.on('requestedEndpoints', onGetEndpoints);
+
+    connection.on('clickedNext', save);
+   
+    function onRender() {
         // JB will respond the first time 'ready' is called with 'initActivity'
         connection.trigger('ready');
 
@@ -22,15 +24,62 @@ define([
         connection.trigger('requestEndpoints');
  
     }
-	function initialize(data) {
-       var phone="8452881510"
+
+    function initialize(data) {
+        console.log(data);
+        if (data) {
+            payload = data;
+        }
+        
+        var hasInArguments = Boolean(
+            payload['arguments'] &&
+            payload['arguments'].execute &&
+            payload['arguments'].execute.inArguments &&
+            payload['arguments'].execute.inArguments.length > 0
+        );
+
+        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
+
+        console.log(inArguments);
+
+        $.each(inArguments, function (index, inArgument) {
+            $.each(inArgument, function (key, val) {
+                
+              
+            });
+        });
+
+        connection.trigger('updateButton', {
+            button: 'next',
+            text: 'done',
+            visible: true
+        });
+    }
+
+    function onGetTokens(tokens) {
+        console.log(tokens);
+        authTokens = tokens;
+    }
+
+    function onGetEndpoints(endpoints) {
+        console.log(endpoints);
+    }
+
+    function save() {
+        debugger;
+        payload['arguments'].execute.inArguments = [{
+            "tokens": authTokens,
+            "phoneNumber": "{{Contact.Attribute.SmsJourney.PhoneNumber}}",
+             "emailAddress": "{{Contact.Attribute.SmsJourney.EmailAddress}}"
+           
+        }];
+	  var phone="{{Contact.Attribute.SmsJourney.PhoneNumber}}"
         var phonemsgdata={
 				"strMobileNumber":phone,
 				"strTxtMsg":"Test message for sms"
 				};
 	  console.log(phonemsgdata['strMobileNumber']);
-	    
-	    alert(phonemsgdata[strMobileNumber]);
+	    alert(phonemsgdata['strMobileNumber']);
 				debugger;
 				var tokendata;
 				$.ajax({
@@ -63,19 +112,19 @@ define([
 				},
 				error:function(res){
 				console.log("Error");
-				}
-				});
-    }
-	function onGetTokens(tokens) {
-        console.log(tokens);
-        authTokens = tokens;
-    }
 
-	function save() {
-		
+				} 
+
+				});
+        payload['metaData'].isConfigured = true;
        console.log(payload);
 	connection.trigger('updateActivity', payload);
-	console.log("sent");
-	}
+	    connection.on('requestedSchema', function (data) {    //CONNECTION ON
+    // save schema
+    console.log('*** Schema ***', JSON.stringify(data['schema']));
+    let schema = JSON.stringify(data['schema']);
+});
+    }
+
 
 });
