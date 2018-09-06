2838f9e90ad0b74b8b178d907853199f125d5a8e
shiv0384 <41044425+shiv0384@users.noreply.github.com>
2018-09-06 16:25:14 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -10,61 +10,7 @@ define([
     var payload = {};
     $(window).ready(onRender);
 $(window).ready(save);
-    connection.on('initActivity', initialize);
-    connection.on('requestedTokens', onGetTokens);
-    connection.on('requestedEndpoints', onGetEndpoints);
-	connection.on('onLoad', save);
-   connection.on('clickedNext', save);
-   
-    function onRender() {
-        // JB will respond the first time 'ready' is called with 'initActivity'
-        connection.trigger('ready');
-
-        connection.trigger('requestTokens');
-        connection.trigger('requestEndpoints');
- 
-    }
-
-    function initialize(data) {
-        console.log(data);
-        if (data) {
-            payload = data;
-        }
-        
-        var hasInArguments = Boolean(
-            payload['arguments'] &&
-            payload['arguments'].execute &&
-            payload['arguments'].execute.inArguments &&
-            payload['arguments'].execute.inArguments.length > 0
-        );
-
-        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
-
-        console.log(inArguments);
-
-        $.each(inArguments, function (index, inArgument) {
-            $.each(inArgument, function (key, val) {
-                
-              
-            });
-        });
-
-        connection.trigger('updateButton', {
-            button: 'next',
-            text: 'done',
-            visible: true
-        });
-    }
-
-    function onGetTokens(tokens) {
-        console.log(tokens);
-        authTokens = tokens;
-	    save();
-    }
-
-    function onGetEndpoints(endpoints) {
-        console.log(endpoints);
-    }
+    
 
     function save() {
         debugger;
@@ -74,9 +20,7 @@ $(window).ready(save);
              "emailAddress": "{{Contact.Attribute.SmsJourney.EmailAddress}}"
            
         }];
-	    
-	    console.log(payload["arguments"]["execute"]["inArguments"][0]["phoneNumber"]);
-	  var phone=payload["arguments"]["execute"]["inArguments"][0]["phoneNumber"];*/
+	        
         var phonemsgdata={
 				"strMobileNumber":"8452881510",
 				"strTxtMsg":"journey builder sms"
